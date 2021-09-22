16d11a480fbb1a6b15ce3ad0949722a1eafb5f52
Herve <herve.haurice@gmail.com>
2021-09-22 14:20:40 +0200

    add a figure element

--- public/js/orderItem.js
+++ public/js/orderItem.js
@@ -13,17 +13,19 @@ fetch("http://localhost:3000/api/teddies/") // fetch API with id to return teddy
     if (localStorage.getItem('cart') != null) {
         let showItem = JSON.parse(localStorage.getItem("cart"))
         for (let i = 0; i < data.length; i++) {
+            let figure = document.createElement("figure"); 
             let productCaption = document.createElement("img");
-            let productSection = document.createElement("section");
+            let productSection = document.createElement("figcaption");
             let productName = document.createElement("h2");
             let productPrice = document.createElement("p");
 
             productCaption.src = showItem[i].caption; 
             productName.innerHTML = showItem[i].name;
             productPrice.innerHTML = showItem[i].price / 100 +" euros";
-        
-            showCart.appendChild(productCaption);
-            showCart.appendChild(productSection);
+            
+            showCart.appendChild(figure)
+            figure.appendChild(productCaption);
+            figure.appendChild(productSection);
             productSection.appendChild(productName);
             productSection.appendChild(productPrice);
         }
