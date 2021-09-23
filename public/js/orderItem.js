aae1ebe08409325840ed940ea4a4883cdf2a6100
Herve <herve.haurice@gmail.com>
2021-09-23 12:34:37 +0200

    Add variable and appends to display color of selected product

--- public/js/orderItem.js
+++ public/js/orderItem.js
@@ -17,16 +17,19 @@ fetch("http://localhost:3000/api/teddies/") // fetch API with id to return teddy
             let productCaption = document.createElement("img");
             let productSection = document.createElement("figcaption");
             let productName = document.createElement("h2");
+            let productColor = document.createElement("p")
             let productPrice = document.createElement("p");
 
             productCaption.src = showItem[i].caption; 
             productName.innerHTML = showItem[i].name;
+            productColor.innerHTML = showItem[i].color;
             productPrice.innerHTML = showItem[i].price / 100 +" euros";
             
             showCart.appendChild(figure)
             figure.appendChild(productCaption);
             figure.appendChild(productSection);
             productSection.appendChild(productName);
+            productSection.appendChild(productColor)
             productSection.appendChild(productPrice);
         }
     } else {
