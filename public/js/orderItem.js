66e29f5899d4a2eacfa1772263165c4b27ec23ed
Herve <herve.haurice@gmail.com>
2021-09-28 08:50:02 +0200

    added condition to display 0 if cart empty and test post method

--- public/js/orderItem.js
+++ public/js/orderItem.js
@@ -9,9 +9,7 @@ fetch("http://localhost:3000/api/teddies/") // fetch API with id to return teddy
 })    
 .then((data) => {
 
-    showCart(data)
-    updatePrice(data)
-     
+    showCart(data)     
 })   
 
 
@@ -19,15 +17,24 @@ function showCart(data) {
     let showCart = document.getElementById("cart");
 
     let showItem = JSON.parse(localStorage.getItem("cart"))
-  
+    if(showItem == null) {
+      
+        let emptyCartMessage = document.createElement("p");
+        showCart.appendChild(emptyCartMessage);
+        emptyCartMessage.textContent = "Votre panier est vide";
+
+    } else {
+
         for (let i = 0; i < data.length; i++) {
+
+
             let figure = document.createElement("figure"); 
             let productCaption = document.createElement("img");
             let productSection = document.createElement("figcaption");
             let productName = document.createElement("h2");
             let productColor = document.createElement("p")
             let productPrice = document.createElement("p");
-
+    
             productCaption.src = showItem[i].caption; 
             productName.innerHTML = showItem[i].name;
             productColor.innerHTML = showItem[i].color;
@@ -41,8 +48,7 @@ function showCart(data) {
             productSection.appendChild(productPrice);
 
         }
-
-        updatePrice(data)
+    }
 }
 
 
@@ -56,28 +62,68 @@ let numberOfItems = document.getElementById("numberOfItems"); // Je cible
 let cartCount = document.createElement("span"); // create span element 
 numberOfItems.appendChild(cartCount); // update it with my local storage
 
-cartCount.textContent = JSON.parse(localStorage.getItem('cart')).length; // fill it with my local storage length
-basketCount.textContent = JSON.parse(localStorage.getItem('cart')).length; // fill it with my local 
+
+
+if (localStorage.getItem('cart') == null) {
+    cartCount.textContent = 0;
+    basketCount.textContent = 0;
+} else {
+    cartCount.textContent = JSON.parse(localStorage.getItem('cart')).length; // fill it with my local storage length
+    basketCount.textContent = JSON.parse(localStorage.getItem('cart')).length; // fill it with my local 
+}
+
 
 function updatePrice(data) {
     let totalPrice = document.getElementById("totalPrice");
     let total = document.createElement("span");
     totalPrice.appendChild(total);
     let showPrice = JSON.parse(localStorage.getItem("cart"));
-    let sum=0;
-    for(let i=0; i < showPrice.length;i++) {
-        let price = showPrice[i].price / 100;
-        sum+=price;
 
-    total.textContent = sum.toFixed(2)+" €";
+    if(showPrice == null) {
+        total.textContent = 0;
+    } else {
+        let sum=0;
+        for(let i=0; i < showPrice.length;i++) {
+            let price = showPrice[i].price / 100;
+            sum+=price;
+        
+            total.textContent = sum.toFixed(2)+" €";
+        }
     }
-}
+  }
 
-updatePrice()
-
-
+  updatePrice();
 
 
 
 
+/* test post method
 
+
+let contact = {
+    firstName: "hello",
+    lastName: "hello",
+    address: "hello",
+    city: "hello",
+    email: "hello"};
+
+console.log(contact)
+
+let getProductId = JSON.parse(localStorage.getItem("cart"));
+let products = getProductId[0].id;
+console.log(products)
+
+
+fetch("http://localhost:3000/api/teddies/order", {
+    method: "POST",
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    body: JSON.stringify({contact, products})
+  
+}).then(res => {
+    console.log(res.json())
+})
+
+*/
