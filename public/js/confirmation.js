e42c6f23dcf30500f14b2dc637445ba4bd5bb0cc
Herve <herve.haurice@gmail.com>
2021-09-30 15:58:06 +0200

    Added link to homepage and comments

--- public/js/confirmation.js
+++ public/js/confirmation.js
@@ -1,3 +1,6 @@
+
+// Function display confirmation with user name, order id and total price set in key confirmation in localstorage
+
 function displayConfirmation(){
     let myMessage = document.getElementById("confirmation");
     let showConfirmation = JSON.parse(localStorage.getItem("confirmation"));
@@ -6,6 +9,8 @@ function displayConfirmation(){
         let orderId = document.createElement("p");
         let totalPrice = document.createElement("b");
         let myCondition = document.createElement("p");
+        let homePageLink = document.createElement("a");
+        let homePageBtn = document.createElement("button");
     
         myName.innerHTML = showConfirmation.name+",";
         orderId.innerHTML = "Nous sommes ravis que vous ayez trouvé votre bonheur et confirmons l'enregistrement de votre commande n° "+showConfirmation.id
@@ -13,16 +18,27 @@ function displayConfirmation(){
         totalPrice.innerHTML = showConfirmation.price;
         totalPrice.style.fontWeight = 600;
         myCondition.innerHTML = "Vous allez recevoir d'ici peu un email de confirmation de commande. Prochaine étape? L'expédition de votre commande! Vous recevrez un email de notre part pour vous prévenir qu'elle est en route."
-    
+        homePageLink.href = ("../index.html")
+        homePageBtn.innerHTML = "Retour à la page d'accueil";
+
         myMessage.append(myName)
         myMessage.append(orderId);
         orderId.appendChild(totalPrice);
-        myMessage.appendChild(myCondition)
+        myMessage.appendChild(myCondition);
+        myMessage.appendChild(homePageLink);
+        homePageLink.appendChild(homePageBtn);
 }
 
 displayConfirmation();
 
+// Set to zero the basket in local storage by deleting cart 
+
+function deleteItem (){
+         localStorage.removeItem("cart")  
+}
+
+deleteItem()
 
 
 
-  
+
